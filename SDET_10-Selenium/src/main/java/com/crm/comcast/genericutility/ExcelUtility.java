da7cf6b7d00273361c8bac5665629cf2c2bd8522
Deepak <Deepak@Ty-Deepak-Tr>
2021-02-26 14:48:23 +0530

    push framework

--- SDET_10-Selenium/src/main/java/com/crm/comcast/genericutility/ExcelUtility.java
+++ SDET_10-Selenium/src/main/java/com/crm/comcast/genericutility/ExcelUtility.java
@@ -9,6 +9,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+
  
 
 /**
@@ -46,6 +47,48 @@ public class ExcelUtility {
 		wb.close();
 	}
 	
+	/**
+	 * used to read the data from excel workbook based on testId & ColumnHeader
+	 * @param sheetName
+	 * @param expTestID
+	 * @param expColHEader
+	 * @return
+	 * @throws Throwable
+	 */
+	public String getExcelData(String sheetName , String expTestID , String expColHEader) throws Throwable {
+		
+				int expTestRow = 0;
+				int expHeader = 0;
+				FileInputStream fis = new FileInputStream(IConstant.excelFilePath);
+				Workbook wb = WorkbookFactory.create(fis);
+				Sheet sh = wb.getSheet(sheetName);
+				int rowCount = sh.getLastRowNum();
+			    
+		    for(int i=0; i<rowCount ; i++) {		
+				Row row = sh.getRow(i);
+				String zeroColData= row.getCell(0).getStringCellValue();
+		        if(expTestID.contentEquals(zeroColData)) {
+		        	//System.out.println("test is availbale");
+		        	expTestRow = i;
+		        	break;
+		        }
+		    }
+		    
+		    int expColHeader = expTestRow-1;
+		    
+		        int colCount = sh.getRow(expColHeader).getLastCellNum();
+		         for(int j=0 ; j <colCount ; j++) {
+		        	   String actColHeader = sh.getRow(expColHeader).getCell(j).getStringCellValue();
+		        	   if(actColHeader.equals(expColHEader)) {
+		        		        //System.out.println("header is avibale ");
+		        		        expHeader = j;
+		        		        break;
+		        	   }
+		         }
+		         
+      return sh.getRow(expTestRow).getCell(expHeader).getStringCellValue();
+				
+	}
 	
 	
 	
